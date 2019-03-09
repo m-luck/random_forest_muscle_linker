79b856a66e80dda3f615cb1830ebffae8ed224b9
unknown <michael.j.lukiman@gmail.com>
2019-03-09 12:20:25 -0500

    RF predicts with 91%

--- rf.py
+++ rf.py
@@ -1,6 +1,117 @@
-import pandas as pd
+#!/usr/bin/env python
+# coding: utf-8
 
+# In[1]:
+
+
+import pandas as pd, numpy as np
 features = pd.read_csv('out.csv')
 features.head(5)
 
 features.describe()
+
+
+# In[2]:
+
+
+# One hot encoding 
+features = pd.get_dummies(features)
+
+
+# In[3]:
+
+
+# Display the first 5 rows of the last 12 columns
+features.iloc[:,:].tail(20)
+
+
+# In[4]:
+
+
+# Labels are the values we want to predict
+labels = np.array(features['w'])
+# Remove the labels from the features
+# axis 1 refers to the columns
+features= features.drop('w', axis = 1)
+# Saving feature names for later use
+feature_list = list(features.columns)
+# Convert to numpy array
+features = np.array(features)
+
+
+# In[5]:
+
+
+# Using Skicit-learn to split data into training and testing sets
+from sklearn.model_selection import train_test_split
+# Split the data into training and testing sets
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
+
+
+# In[6]:
+
+
+print('Training Features Shape:', train_features.shape)
+print('Training Labels Shape:', train_labels.shape)
+print('Testing Features Shape:', test_features.shape)
+print('Testing Labels Shape:', test_labels.shape)
+
+
+# In[7]:
+
+
+# Import the model we are using
+from sklearn.ensemble import RandomForestRegressor
+# Instantiate model with 1000 decision trees
+rf = RandomForestRegressor(n_estimators = 1020, random_state = 42)
+# Train the model on training data
+rf.fit(train_features, train_labels);
+
+
+# In[8]:
+
+
+# Use the forest's predict method on the test data
+predictions = rf.predict(test_features)
+# Calculate the absolute errors
+errors = abs(predictions - test_labels)
+# Print out the mean absolute error (mae)
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+
+
+# In[9]:
+
+
+# Calculate mean absolute percentage error (MAPE)
+mape = 100 * (errors / test_labels)
+# Calculate and display accuracy
+accuracy = 100 - np.mean(mape)
+print('Accuracy:', round(accuracy, 2), '%.')
+
+
+# In[10]:
+
+
+# Import tools needed for visualization
+from sklearn.tree import export_graphviz
+import pydot
+# Pull out one tree from the forest
+tree = rf.estimators_[5]
+# Import tools needed for visualization
+from sklearn.tree import export_graphviz
+import pydot
+# Pull out one tree from the forest
+tree = rf.estimators_[5]
+# Export the image to a dot file
+export_graphviz(tree, out_file = 'tree.dot', feature_names = feature_list, rounded = True, precision = 1)
+# Use dot file to create a graph
+(graph, ) = pydot.graph_from_dot_file('tree.dot')
+# Write graph to a png file
+graph.write_png('tree.png')
+
+
+# In[ ]:
+
+
+
+
